96c6c0eeed8446a712f6f33394c10e4ce17390fe
Freelancer21 <freelancer112117@gmail.com>
2024-01-10 18:04:33 +0530

    minor change

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -3,6 +3,8 @@ import User from "../database/models/user";
 export const addUser = async (req: any, res: any) => {
   try {
     const { name, cityId, phoneNo } = req.body;
+    console.log("addUser");
+
     const user = await User.create({ name, cityId, phoneNo });
     return res.status(201).json({ user });
   } catch (error: any) {
